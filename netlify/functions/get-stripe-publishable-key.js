4b7a91bc74328539e8ffc5d440464024b118c91f
prettybites14-png <prettybites14@gmail.com>
2026-03-17 16:29:21 -0400

    Delete netlify directory

--- netlify/functions/get-stripe-publishable-key.js
+++
@@ -1,5 +0,0 @@
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ publishableKey: process.env.STRIPE_PUBLISHABLE_KEY || '' })
-});
